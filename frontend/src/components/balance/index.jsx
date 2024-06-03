36f8ad9794501da8b9514c29f89d5ce3822fd5ed
Dasher <dasha.topor@gmail.com>
2024-06-04 00:10:30 +0200

    fix: display balance

--- frontend/src/components/balance/index.jsx
+++ frontend/src/components/balance/index.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function Balance( {title, spend, description} ){
+export default function Balance( {title, amount, description} ){
     return (
         <section className="account">
         <div className="account-content-wrapper">
           <h3 className="account-title">{title}</h3>
-          <p className="account-amount">{spend}</p>
+          <p className="account-amount">{amount}</p>
           <p className="account-amount-description">{description}</p>
         </div>
         <div className="account-content-wrapper cta">
